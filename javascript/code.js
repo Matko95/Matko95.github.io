0884d129fa474ef59ffe195a19097b541a87aef2
Matko95 <mtrifunovski@gmail.com>
2015-11-14 01:57:59 +0100

    rev 2.0

---
+++ javascript/code.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $(".img, .shadow").hover(function(){
+        $(this).css({"top" : "-5px","box-shadow": "14px 14px 14px #777"});
+        }, function(){
+        $(this).css({"top":"+5px","box-shadow": "7px 7px 7px #666"});
+    });
+ 
+  
+  $("#aboutBtn").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#about").offset().top},
+        'slow');
+});
+  
+  $("#homeBtn").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#home").offset().top},
+        'slow');
+});
+  $("#portfolioBtn").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#portfolio").offset().top},
+        'slow');
+});
+  
+  $("#contactBtn").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#contact").offset().top},
+        'slow');
+});
+
+});
+    
+
